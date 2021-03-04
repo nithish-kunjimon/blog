a0c27ef96afd302d2bbe67be6358632b555ee12b
nithish-kunjimon <nithishak22@gmail.com>
2021-03-04 20:02:41 +0100

    First Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const articleRouter = require("./routes/articles");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const methodOverride = require("method-override");
+const app = express();
+
+const url = process.env.DB_URL || "mongodb://localhost/blog";
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
+  res.render("articles/index", {
+    articles: articles,
+  });
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(3000);
